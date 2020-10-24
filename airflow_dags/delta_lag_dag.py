3320c60dc4babb514dcc7a50125ced8c36f15f6a
mahdi.nematpour <mahdi.nematpour@pintapin.com>
2020-10-25 01:01:21 +0330

    added pyspark job and airflow dag

---
+++ airflow_dags/delta_lag_dag.py
@@ -0,0 +1,37 @@
+from datetime import datetime, timedelta
+
+import pendulum
+from airflow import DAG
+from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
+from airflow.models import Variable
+
+local_tz = pendulum.timezone("Asia/Tehran")
+
+default_args = {
+    'owner': 'mahdyne',
+    'depends_on_past': False,
+    'start_date': datetime(2020, 10, 10, tzinfo=local_tz),
+    'email': ['nematpour.ma@gmail.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=5)
+}
+dag = DAG(dag_id='delta_lag_dag',
+          default_args=default_args,
+          catchup=False,
+          schedule_interval="30 * * * *")
+pyspark_app_home=Variable.get("PYSPARK_APP_HOME")
+delta_lag_alert= SparkSubmitOperator(task_id='clickstream_delta_lag_alert',
+                                     conn_id='spark_local',
+                                     application=f'{pyspark_app_home}/spark/delta_lag.py',
+                                     total_executor_cores=4,
+                                     packages="io.delta:delta-core_2.12:0.7.0",
+                                     executor_cores=2,
+                                     executor_memory='5g',
+                                     driver_memory='5g',
+                                     name='clickstream_delta_lag_alert',
+                                     dag=dag,
+                                     application_args=["/home/mahdyne/wd/data/spark_streaming/click_stream","searched_at","12000"]
+                                     )
+
---
+++ spark/delta_lag.py
@@ -0,0 +1,33 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col,max
+import datetime
+import sys
+class LagException(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+    def __str__(self):
+        return repr(self.msg)
+sparkSession = SparkSession.builder \
+    .config("spark.driver.maxResultSize", "2000m") \
+    .config("spark.sql.shuffle.partitions", 4) \
+    .getOrCreate()
+sparkSession.sparkContext.setLogLevel('WARN')
+def check_lag(delta_path:str,timestamp_column:str,threshold_in_min:int):
+    df = sparkSession.read.format("delta").load(delta_path)
+    latest_ts=df.select(max(col(timestamp_column))).first()[0]
+    now=datetime.datetime.now()
+    lag=now-latest_ts
+    lag_in_min=divmod(lag.total_seconds(), 60)[0]
+    if lag_in_min>threshold_in_min:
+        raise LagException(f'The lag of dataset {delta_path} was {lag_in_min} min when checked at {now}')
+    else:
+        print(f"No lag in dataset {delta_path} with specified threshold {threshold_in_min}")
+    sparkSession.stop()
+def main():
+    delta_path=sys.argv[1]
+    timestamp_column=sys.argv[2]
+    threshold_in_min=int(sys.argv[3])
+    check_lag(delta_path,timestamp_column,threshold_in_min)
+
+if __name__ == '__main__':
+    main()
