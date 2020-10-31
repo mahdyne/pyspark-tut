432786d4cc2987405ba0cc7b5605ee4c50b640c0
mahdi.nematpour <mahdi.nematpour@pintapin.com>
2020-10-31 10:33:02 +0330

    added responsed_at

--- spark/search_event_ingestor.py
+++ spark/search_event_ingestor.py
@@ -1,12 +1,13 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, from_json, date_format
-from pyspark.sql.types import StringType,StructType,StructField,DateType
+from pyspark.sql.types import StringType, StructType, StructField, DateType, TimestampType
+
 sparkSession = SparkSession.builder \
     .config("spark.driver.maxResultSize", "2000m") \
     .config("spark.sql.shuffle.partitions", 4) \
     .getOrCreate()
-delta_path="hdfs://192.168.1.10:9820/delta/stream/flight_search"
-checkpoint_url= "hdfs://192.168.1.10:9820/delta/stream/checkpoints/flight_search"
+delta_path="hdfs://192.168.1.10:9820/delta/stream/search_response"
+checkpoint_url= "hdfs://192.168.1.10:9820/delta/stream/checkpoints/search_response"
 def ingest_search_event():
     stream=sparkSession\
         .readStream.format("kafka")\
@@ -15,14 +16,17 @@ def ingest_search_event():
         .option("startingOffsets", "earliest")\
         .load()
     schema=StructType([
+        StructField("searched_at",TimestampType()),
+        StructField("responsed_at", TimestampType()),
         StructField("channel",StringType()),
         StructField("origin",StringType()),
         StructField("destination", StringType()),
         StructField("departure_date",DateType())
     ])
     stream\
-        .select(col("timestamp").alias("searched_at"),from_json(col("value").cast(StringType()),schema).alias("json"))\
-        .select(col("searched_at"),
+        .select(from_json(col("value").cast(StringType()),schema).alias("json"))\
+        .select(col("json.searched_at").alias("searched_at"),
+                col("json.responsed_at").alias("responsed_at"),
                 col("json.channel").alias("channel"),
                 col("json.origin").alias("origin"),
                 col("json.destination").alias("destination"),
