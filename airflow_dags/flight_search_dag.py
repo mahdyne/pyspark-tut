adcd5e2cfd0bb39578362e2cef479d37c49f7103
mahdi.nematpour <mahdi.nematpour@pintapin.com>
2020-11-26 19:06:36 +0330

    added execution_timeout attr

--- airflow_dags/flight_search_dag.py
+++ airflow_dags/flight_search_dag.py
@@ -1,5 +1,4 @@
 from datetime import datetime, timedelta
-
 import pendulum
 from airflow import DAG
 from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
@@ -21,41 +20,44 @@ dag = DAG(dag_id='flight_search_dag',
           default_args=default_args,
           catchup=False,
           schedule_interval="0 * * * *")
-pyspark_app_home=Variable.get("PYSPARK_APP_HOME")
+pyspark_app_home = Variable.get("PYSPARK_APP_HOME")
 
-flight_search_ingestion= SparkSubmitOperator(task_id='flight_search_ingestion',
-                                     conn_id='spark_local',
-                                     application=f'{pyspark_app_home}/spark/search_event_ingestor.py',
-                                     total_executor_cores=4,
-                                     packages="io.delta:delta-core_2.12:0.7.0,org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0",
-                                     executor_cores=2,
-                                     executor_memory='5g',
-                                     driver_memory='5g',
-                                     name='flight_search_ingestion',
-                                     dag=dag
-                                     )
+flight_search_ingestion = SparkSubmitOperator(task_id='flight_search_ingestion',
+                                              conn_id='spark_local',
+                                              application=f'{pyspark_app_home}/spark/search_event_ingestor.py',
+                                              total_executor_cores=4,
+                                              packages="io.delta:delta-core_2.12:0.7.0,org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0",
+                                              executor_cores=2,
+                                              executor_memory='5g',
+                                              driver_memory='5g',
+                                              name='flight_search_ingestion',
+                                              execution_timeout=timedelta(minutes=10),
+                                              dag=dag
+                                              )
 
-flight_search_waiting_time= SparkSubmitOperator(task_id='flight_search_waiting_time',
-                                     conn_id='spark_local',
-                                     application=f'{pyspark_app_home}/spark/flight_search_waiting_time.py',
-                                     total_executor_cores=4,
-                                     packages="io.delta:delta-core_2.12:0.7.0,org.postgresql:postgresql:42.2.9",
-                                     executor_cores=2,
-                                     executor_memory='10g',
-                                     driver_memory='10g',
-                                     name='flight_search_waiting_time',
-                                     dag=dag
-                                     )
+flight_search_waiting_time = SparkSubmitOperator(task_id='flight_search_waiting_time',
+                                                 conn_id='spark_local',
+                                                 application=f'{pyspark_app_home}/spark/flight_search_waiting_time.py',
+                                                 total_executor_cores=4,
+                                                 packages="io.delta:delta-core_2.12:0.7.0,org.postgresql:postgresql:42.2.9",
+                                                 executor_cores=2,
+                                                 executor_memory='10g',
+                                                 driver_memory='10g',
+                                                 name='flight_search_waiting_time',
+                                                 execution_timeout=timedelta(minutes=10),
+                                                 dag=dag
+                                                 )
 
-flight_nb_search= SparkSubmitOperator(task_id='flight_nb_search',
-                                     conn_id='spark_local',
-                                     application=f'{pyspark_app_home}/spark/nb_search.py',
-                                     total_executor_cores=4,
-                                     packages="io.delta:delta-core_2.12:0.7.0,org.postgresql:postgresql:42.2.9",
-                                     executor_cores=2,
-                                     executor_memory='10g',
-                                     driver_memory='10g',
-                                     name='flight_nb_search',
-                                     dag=dag
-                                     )
-flight_search_ingestion>>[flight_search_waiting_time,flight_nb_search]
+flight_nb_search = SparkSubmitOperator(task_id='flight_nb_search',
+                                       conn_id='spark_local',
+                                       application=f'{pyspark_app_home}/spark/nb_search.py',
+                                       total_executor_cores=4,
+                                       packages="io.delta:delta-core_2.12:0.7.0,org.postgresql:postgresql:42.2.9",
+                                       executor_cores=2,
+                                       executor_memory='10g',
+                                       driver_memory='10g',
+                                       name='flight_nb_search',
+                                       execution_timeout=timedelta(minutes=10),
+                                       dag=dag
+                                       )
+flight_search_ingestion >> [flight_search_waiting_time, flight_nb_search]
